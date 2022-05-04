4ee6067ece0443e07cee197662acd32cc0b8560c
Herickow <102757332+Herickow@users.noreply.github.com>
2022-05-04 16:50:39 -0300

    Add files via upload

---
+++ 1-codigo.js
@@ -0,0 +1,36 @@
+console.log(`Iaer Worldd`);
+console.log(7+4);
+console.log('7'+'4');
+
+
+
+console.log(12+20);
+console.log(25+157);
+console.log(30+16);
+
+
+
+console.log('Hericko'+ 'Jr');
+console.log('Hericko'- 'Jr');
+
+console.log('Hericko '+ 'Jr');
+
+
+
+const nome = ('Hericko');
+console.log(nome)
+
+
+
+const a = 2;
+const b = 3;
+
+console.log(a+b+25);
+
+
+
+
+const c= 1500;
+const d= 1200;
+
+console.log(c+d);
